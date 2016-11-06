008a14a553eec5cbd8b68ad0bd09bede2080d440
Niti Nrw <niti.patel@flatironschool.com>
2016-11-06 13:18:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const app = "I don't do much.";
+const addInitialsToHeroes = heroes => heroes.map(hero => Object.assign({}, hero, {
+  initials: hero.firstName.charAt(0) + hero.lastName.charAt(0),
+}));
+
+//to make sure we don't modify original array
